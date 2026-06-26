70f601b0da7bb69bbaca984262a845311478fa54
Okeke Chukwudalu <okekechukwudalu92@gmail.com>
2026-06-26 18:02:59 +0100

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-import Landing from "../pages/Landing"
-import Auth from "../pages/Auth"
-import StudentDashboard from "../pages/StudentDashboard"
-import DriverDashboard from "../pages/DriverDashboard"
+import Landing from "../pages/Landing";
+import Auth from "../pages/Auth";
+import StudentDashboard from "../pages/StudentDashboard";
+import DriverDashboard from "../pages/DriverDashboard";
 
 export default function AppRoutes() {
-    return(
-        <Routes>
-           <Route path="/" element={<Landing />} />
-           <Route path="/auth" element={<Auth />} />
-           
-        </Routes>
-    )
+  const { user } = useAuth();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Auth />} />
+      
+      <Route
+        path="/student"
+        element={user?.role === "student" ? <StudentDashboard /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/driver"
+        element={user?.role === "driver" ? <DriverDashboard /> : <Navigate to="/auth" />}
+      />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
