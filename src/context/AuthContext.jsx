f94c59b360c7fb342d88520ab2db27d5a7b8bb53
Okeke Chukwudalu <okekechukwudalu92@gmail.com>
2026-06-26 18:03:31 +0100

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,20 +3,25 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-    const[user, setUser] = useState(null);
-    //user shhape : shape{name, phone, role:'student' | 'driver' }
-    const mockLogin = (name, phone, role) => {
-        setUser({name, phone, role});
-    };
-    const logout = () => setUser(null);
+  const [user, setUser] = useState(null);
 
-    return(
-        <AuthContext.Provider value={{user, mockLogin,logout }}>
-            { children }
-        </AuthContext.Provider>
-    );
+  const mockLogin = (name, phone, role) => {
+    setUser({ name, phone, role });
+  };
+
+  const logout = () => setUser(null);
+
+  return (
+    <AuthContext.Provider value={{ user, mockLogin, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function useAuth(){
-    return useContext(AuthContext);
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
 }
